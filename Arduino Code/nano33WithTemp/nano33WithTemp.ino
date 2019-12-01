c2daee60f32ad4dce217edc907ad6c8edc6453d7
Nicholas English <35505237+NWEenglish@users.noreply.github.com>
2019-12-01 14:23:47 -0500

    Nano now sends data to the server over WiFi.

--- Arduino Code/nano33WithTemp/nano33WithTemp.ino
+++ Arduino Code/nano33WithTemp/nano33WithTemp.ino
@@ -12,7 +12,14 @@
  * Due Date: December 4, 2019
  *********************************************************************/
 
+
 #include <math.h>
+#include <stdio.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+/********** Temp Sensor Values **********/
 
 /* B value of the thermistor */
 const int B = 4275;
@@ -26,6 +33,22 @@ const int pinTempSensor = A0;
 /* Holds the tempurature in Celsius */
 float temp;
 
+
+/********** WiFi Values **********/
+
+int status = WL_IDLE_STATUS;
+
+char ssid[] = /* REDACTED */;   // REMEMBER TO REMOVE PERSONAL INFO BEFORE PUSH
+char pass[] = /* REDACTED */;   // !!!
+
+unsigned int localPort = 2390;
+
+/* A string to send to the server. */
+char MessageBuffer[] = "";
+
+WiFiUDP Udp;
+
+
 /**********************************************************************
  * 
  * The setup function before we go into the loop.
@@ -33,7 +56,48 @@ float temp;
  *********************************************************************/
 void setup() {
 
+    /* Used to initialize the serial and wait for a port to open. */
     Serial.begin(9600);
+
+
+    /* Check for the WiFi module. */
+    if (WiFi.status() == WL_NO_MODULE) {
+      
+      Serial.println("Communication with WiFi module failed!");
+
+      /* Then do NOT continue. */
+      while (true);
+    }
+
+
+    /* Check the firmware version. */
+    String fv = WiFi.firmwareVersion();
+    
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+      Serial.println("Please upgrade the firmware");
+
+
+    /* We attempt to connect to the WiFi network. */
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
+  
+      /* We wait 10 seconds for a connection. */
+      Serial.println("Connecting to wifi...");
+      delay(10000);
+    }
+
+
+
+    Serial.println("Connected to wifi!\n");
+    printWifiStatus();
+  
+    Serial.println("\nStarting transmission to server...");
+    Udp.begin(localPort);
+    
 }
 
 /**********************************************************************
@@ -44,15 +108,29 @@ void setup() {
  *********************************************************************/
 void loop() {
 
+    /* Get the current temp from the sensor with 2 decimal places. */
     temp = getTemp();
+    sprintf(MessageBuffer, "%.2f", temp);
 
-    delay(100);
+    
+    /* Send a message to the computer with the server. */
+    IPAddress destinationIP( /* REDACTED */ );  // Address of target machine  (Private IP)
+    unsigned int destinationPort = 10000;      // Port to send to
+    
+    Udp.beginPacket(destinationIP, destinationPort);
+
+    Udp.write(MessageBuffer);
+    Udp.endPacket();
+
+
+    /* Delay between loops. */
+    delay(1000);
 }
 
 
 
 /**********************************************************************
- * The functions gets the tempurature from a Grove Temp Sensor v1.2.
+ * The function gets the tempurature from a Grove Temp Sensor v1.2.
  * The function was provided by Seeed, the creator of the sensor.
  * 
  * http://wiki.seeedstudio.com/Grove-Temperature_Sensor_V1.2/
@@ -72,4 +150,29 @@ float getTemp() {
 
     return temp;
   
+}
+
+
+
+/**********************************************************************
+ * The function prints the WiFi status that the Nano connected to.
+ * The function was provided by Arduino.
+ * 
+ * https://www.arduino.cc/en/Tutorial/WiFiNINAWiFiUdpSendReceiveString
+ *********************************************************************/
+void printWifiStatus() {
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+  
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+  
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
 }
